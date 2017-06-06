05a3bd696d54a9abaf40f335dfd56dfecc25a928
Cesar Viana <cesar.pviana@gmail.com>
2017-06-06 10:34:47 -0300

    Criando listenener movimento jogador

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,17 @@
 package main;
 
-import agent.Jogo;
+import java.awt.Point;
+
+import jogo.Campo;
+import jogo.TesteListener;
 
 public class Main {
-	public static void main(String args[]){
-		new Jogo().inicia();
+	public static void main(String args[]) {
+		Campo campo = new Campo();
+		campo.adicionaJogador("1", "A", new Point(0, 0));
+
+		campo.addListener(new TesteListener()); // TODO apagar
+
+		campo.bolaEmJogo();
 	}
 }
--- test/agent/JogoTest.java
+++ test/agent/JogoTest.java
@@ -1,12 +1,17 @@
 package agent;
 
+import java.awt.Point;
+
 import org.junit.Test;
+
+import jogo.Campo;
 
 public class JogoTest {
 
 	@Test
 	public void adiciona_jogador() {
-		Jogo jogo = new Jogo();
-		jogo.adicionaJogador("Robinho");
-	} 
+		Campo campo = new Campo();
+		campo.adicionaJogador("Robinho", "Brasil", new Point(0, 0));
+	}
+
 }
